9139e0d15c28ebc5396d28435235e7fc682d3b89
Jose Nunez <jose.nunez@intel.com>
2014-08-19 21:16:22 -0600

    Basic IOT Log Functionality

---
+++ iot_engine/be/be_DeviceStatus.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This business entity describes the status of pinout of a IOT device
+ *
+ * @author janunezc
+ */
+class be_DeviceStatus {
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G01M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G01V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G02M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G02V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G03M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G03V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G04M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G04V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G05M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G05V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G06M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G06V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G07M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G07V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G08M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G08V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G09M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G09V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G10M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G10V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G11M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G11V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G12M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G12V = '';
+
+    /**     * G##M Specify the mode of the GPIO Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $G13M = '';
+
+    /**     * G##V Specify the value of the GPIO Pin     * @var integer      */ 
+    public $G13V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A0M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A0V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A1M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A1V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A2M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A2V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A3M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A3V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A4M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A4V = '';
+
+    /**     * A##M Specify the mode of the ADC Pin (INPUT, OUTPUT)     * @var String      */ 
+    public $A5M = '';
+
+    /**     * A##V Specify the value of the ADC Pin      * @var Integer      */ 
+    public $A5V = '';
+
+    /**     * Date Time of the device status condition     * @var String      */ 
+    public $datetime = '';
+
+}
--- iot_engine/iot_request.php
+++ iot_engine/iot_request.php
@@ -1,61 +1,65 @@
 <?php
+/**
+ * http://localhost:8080/iot_engine/iot_request.php?DID=GALILEO1&CMD=LOG&USR=ROOT&PWD=&G01M=OUTPUT&G02M=OUTPUT&G03M=OUTPUT&G04M=OUTPUT&G05M=OUTPUT&G06M=OUTPUT&G07M=OUTPUT&G08M=OUTPUT&G09M=OUTPUT&G10M=OUTPUT&G11M=OUTPUT&G12M=OUTPUT&G13M=OUTPUT&G01V=0&G02V=0&G03V=0&G04V=0&G05V=0&G06V=0&G07V=0&G08V=0&G09V=0&G10V=0&G11V=0&G12V=0&G13V=0&A0M=OUTPUT&A0V=0&A1M=OUTPUT&A1V=0&A2M=OUTPUT&A2V=0&A3M=OUTPUT&A3V=0&A4M=OUTPUT&A4V=0&A5M=OUTPUT&A5V=0
+ */
 
+require_once './be/be_DeviceStatus.php';
 require_once './dal/da_conf.php';
 require_once './dal/da_helper.php';
 require_once './dal/da_iot_request.php';
 
-$request = new stdClass();;
-$request->device_id = "GALILEO-01";
-$request->device_command = "LOG";
 
-$request->iot_user = "joe";
-$request->iot_pwd = "doe";
-        
-$request->pin01_mode = "INPUT";
-$request->pin01_value = "1";
+$request = new be_DeviceStatus();
 
-$request->pin02_mode = "INPUT";
-$request->pin02_value = "0";
+$request->DID = filter_input(INPUT_GET, "DID");
+$request->CMD = filter_input(INPUT_GET, "CMD");
 
-$request->pin03_mode = "INPUT";
-$request->pin03_value = "0";
+$request->USR = filter_input(INPUT_GET, "USR");
+$request->PWD = filter_input(INPUT_GET, "PWD");
 
-$request->pin04_mode = "INPUT";
-$request->pin04_value = "0";
+$request->G01M = filter_input(INPUT_GET, 'G01M');
+$request->G01V = filter_input(INPUT_GET, 'G01V');
+$request->G02M = filter_input(INPUT_GET, 'G02M');
+$request->G02V = filter_input(INPUT_GET, 'G02V');
+$request->G03M = filter_input(INPUT_GET, 'G03M');
+$request->G03V = filter_input(INPUT_GET, 'G03V');
+$request->G04M = filter_input(INPUT_GET, 'G04M');
+$request->G04V = filter_input(INPUT_GET, 'G04V');
+$request->G05M = filter_input(INPUT_GET, 'G05M');
+$request->G05V = filter_input(INPUT_GET, 'G05V');
+$request->G06M = filter_input(INPUT_GET, 'G06M');
+$request->G06V = filter_input(INPUT_GET, 'G06V');
+$request->G07M = filter_input(INPUT_GET, 'G07M');
+$request->G07V = filter_input(INPUT_GET, 'G07V');
+$request->G08M = filter_input(INPUT_GET, 'G08M');
+$request->G08V = filter_input(INPUT_GET, 'G08V');
+$request->G09M = filter_input(INPUT_GET, 'G09M');
+$request->G09V = filter_input(INPUT_GET, 'G09V');
+$request->G10M = filter_input(INPUT_GET, 'G10M');
+$request->G10V = filter_input(INPUT_GET, 'G10V');
+$request->G11M = filter_input(INPUT_GET, 'G11M');
+$request->G11V = filter_input(INPUT_GET, 'G11V');
+$request->G12M = filter_input(INPUT_GET, 'G12M');
+$request->G12V = filter_input(INPUT_GET, 'G12V');
+$request->G13M = filter_input(INPUT_GET, 'G13M');
+$request->G13V = filter_input(INPUT_GET, 'G13V');
 
-$request->pin05_mode = "INPUT";
-$request->pin05_value = "0";
+$request->A0M = filter_input(INPUT_GET, 'A0M');
+$request->A0V = filter_input(INPUT_GET, 'A0V');
+$request->A1M = filter_input(INPUT_GET, 'A1M');
+$request->A1V = filter_input(INPUT_GET, 'A1V');
+$request->A2M = filter_input(INPUT_GET, 'A2M');
+$request->A2V = filter_input(INPUT_GET, 'A2V');
+$request->A3M = filter_input(INPUT_GET, 'A3M');
+$request->A3V = filter_input(INPUT_GET, 'A3V');
+$request->A4M = filter_input(INPUT_GET, 'A4M');
+$request->A4V = filter_input(INPUT_GET, 'A4V');
+$request->A5M = filter_input(INPUT_GET, 'A5M');
+$request->A5V = filter_input(INPUT_GET, 'A5V');
 
-$request->pin06_mode = "INPUT";
-$request->pin06_value = "0";
-
-$request->pin07_mode = "INPUT";
-$request->pin07_value = "0";
-
-$request->pin08_mode = "INPUT";
-$request->pin08_value = "0";
-
-$request->pin09_mode = "INPUT";
-$request->pin09_value = "0";
-
-$request->pin10_mode = "INPUT";
-$request->pin10_value = "0";
-
-$request->pin11_mode = "INPUT";
-$request->pin11_value = "0";
-
-$request->pin12_mode = "INPUT";
-$request->pin12_value = "0";
-
-$request->pin13_mode = "INPUT";
-$request->pin13_value = "0";
 $d1 = new DateTime();
 $request->datetime = $d1->format("Y-m-d H:i:s.u");
 
-$response = $request;
+da_iot_request::iot_request_LogEntry($request);
 
-$response->response_message = "Thanks. " . $d1->format("Y-m-d H:i:s.u");
-
-da_iot_request::iot_request_response_log_LogEntry($request, $response);
-
-echo json_encode($response);
+echo json_encode($request);
